08e59f1db6075e448be8cedadad74af3c11e1b02
paul090181 <paul090181@yahoo.com>
2026-09-02 14:11:53 -0400

    Update upload_to_drive.py
    Fix Google Drive access scope

--- upload_to_drive.py
+++ upload_to_drive.py
@@ -28,7 +28,7 @@ credentials_info = json.loads(
 
 credentials = service_account.Credentials.from_service_account_info(
     credentials_info,
-    scopes=["https://www.googleapis.com/auth/drive.file"],
+    scopes=["https://www.googleapis.com/auth/drive"],
 )
 
 drive = build(
